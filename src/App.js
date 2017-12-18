3f35eec6f3fca3f3e5a6f82b4247de4f21ceadac
cs-300 <halfmoonvic@gmail.com>
2017-12-18 18:57:56 +0800

    release(v1): Hello World

--- src/App.js
+++ src/App.js
@@ -1,41 +1,20 @@
 /**** React应用依赖组件 ****/
 // core
 import React, { Component } from 'react'
-// router
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 /******* 第三方 组件库 *****/
 /**** 本地公用变量 公用函数 **/
 /******* 本地 公用组件 *****/
 /**** 当前组件的 子组件等 ***/
-import TestRedux from './store/test-redux.js'
+import Hello from 'container/hello/hello'
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <Switch>
-          <Redirect path='/' exact to='/testredux'></Redirect>
-          <Route path='/testredux' component={TestRedux}></Route>
-          <Route path='/hello' component={Hello}></Route>
-        </Switch>
-      </Router>
+      <div className="app">
+        <Hello></Hello>
+      </div>
     )
   }
 }
 
 export default App
-
-class Hello extends Component {
-  render() {
-    return (
-      <div>
-        <p>你好啊，格里高利</p>
-        <button onClick={
-          () => {
-            this.props.history.push('/testredux')
-          }
-        }>goto hello</button>
-      </div>
-    )
-  }
-}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import registerServiceWorker from './registerServiceWorker';
-// Redux 相关
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
-import reducers from 'store'
 /******* 第三方 组件库 *****/
 /**** 本地公用变量 公用函数 **/
 /******* 本地 公用组件 *****/
@@ -15,15 +10,8 @@ import 'common/css/index.scss'
 /**** 当前组件的 子组件等 ***/
 import App from './App'
 
-const store = createStore(reducers, compose(
-  applyMiddleware(thunk),
-  window.devToolsExtension ? window.devToolsExtension() : () => {}
-))
-
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <App />,
   document.getElementById('root')
 );
 
