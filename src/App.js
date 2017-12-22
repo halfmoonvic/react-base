bee296faf3a6cd61463532bb730e394c584d50e9
cs-300 <halfmoonvic@gmail.com>
2017-12-22 16:25:01 +0800

    release: state 和 setState()

--- src/App.js
+++ src/App.js
@@ -7,10 +7,24 @@ import React, { Component } from 'react'
 /**** 当前组件的 子组件等 ***/
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      text: '你好啊，格里高利'
+    }
+
+    this.changeText = this.changeText.bind(this)
+  }
+  changeText() {
+    this.setState({
+      text: '被改变啦'
+    })
+  }
   render() {
     return (
       <div className="app">
-        <div>Hello World</div>
+        <div>{this.state.text}</div>
+        <button onClick={this.changeText}>setState 改变 state 值</button>
       </div>
     )
   }
