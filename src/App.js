8bcf99821304176db0ae279619e81db64cebc4e7
cs-300 <halfmoonvic@gmail.com>
2017-12-22 15:48:29 +0800

    fix(release v1): hello world 简化下

--- src/App.js
+++ src/App.js
@@ -6,66 +6,11 @@ import React, { Component } from 'react'
 /******* 本地 公用组件 *****/
 /**** 当前组件的 子组件等 ***/
 
-class Test extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      stateChange: ''
-    }
-    console.log('@init=0. constructor')
-  }
-  componentWillMount() {
-    console.log('@init=1. componentWillMount')
-  }
-  componentDidMount() {
-    console.log('@init=2. componentDidMount')
-  }
-  componentWillReceiveProps(nextProps) {
-    console.log('@change=1. componentWillReceiveProps', nextProps)
-  }
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('@change=2. shouldComponentUpdate', nextProps, nextState)
-    return true
-  }
-  componentWillUpdate() {
-    console.log('@change=3. componentWillUpdate')
-  }
-  componentDidUpdate() {
-    console.log('@change=4. componentDidUpdate')
-  }
-  componentWillUnmount() {
-    console.log('componentWillUnmount')
-  }
-  render() {
-    console.log('   render    ')
-    return (
-      <div>
-        <h2 onClick={() => {
-          this.setState({
-            stateChange: 'changeState'
-          })
-        }}>改变本地 state</h2>
-      </div>
-    )
-  }
-}
-
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      propChange: ''
-    }
-  }
   render() {
     return (
-      <div>
-        <h2 onClick={() => {
-          this.setState({
-            propChange: 'changeProps'
-          })
-        }}>app组件主动更改穿过去的 props</h2>
-        <Test passedAppValue={this.state.propChange} ></Test>
+      <div className="app">
+        <div>Hello World</div>
       </div>
     )
   }
