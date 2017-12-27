6072aa318f06c526f023757538b0678f5e017774
cs-300 <halfmoonvic@gmail.com>
2017-12-27 13:32:31 +0800

    release(v9): 路由

--- src/App.js
+++ src/App.js
@@ -1,71 +1,28 @@
-/**** React应用依赖组件 ****/
-// core
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom'
 
-class Test extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      val: ''
-    }
-    console.log('@init=0. constructor')
-  }
-  componentWillMount() {
-    console.log('@init=1. componentWillMount')
-  }
-  componentDidMount() {
-    console.log('@init=2. componentDidMount')
-  }
-  componentWillReceiveProps(nextProps) {
-    console.log('@change=1. componentWillReceiveProps', nextProps)
-  }
-  shouldComponentUpdate(nextProps, nextState) {
-    console.log('@change=2. shouldComponentUpdate', nextProps, nextState)
-    return true
-  }
-  componentWillUpdate() {
-    console.log('@change=3. componentWillUpdate')
-  }
-  componentDidUpdate() {
-    console.log('@change=4. componentDidUpdate')
-  }
-  componentWillUnmount() {
-    console.log('componentWillUnmount')
-  }
-  render() {
-    console.log('@render @render')
-    return (
-      <div>
-        <button onClick={() => {
-          this.setState({
-            val: 'hehe'
-          })
-        }}>改变本地 state</button>
-      </div>
-    )
-  }
-}
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      val: ''
-    }
-  }
   render() {
     return (
-      <div className="app">
-        <button onClick={() => {
-          this.setState({
-            val: 'change'
-          })
-        }}>app组件主动更改穿过去的 props</button>
-        <br/><br/>
-        <Test name={this.state.val} ></Test>
-      </div>
+      <Router>
+        <div className="app">
+          <Link to="/about" className="route">关于</Link>
+          <br/>
+          <Link to="/mine" className="route">个人主页</Link>
+          <br/><hr/><br/>
+          <Switch>
+            <Redirect path='/' exact to='/about'></Redirect>
+            <Route path="/about" component={About}></Route>
+            <Route path="/mine" component={Mine}></Route>
+          </Switch>
+        </div>
+      </Router>
     )
   }
 }
 
 export default App
+
+const About = (props) => <h1>关于页面</h1>
+const Mine = (props) => <h1>个人主页页面</h1>
