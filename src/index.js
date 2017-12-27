c5d26291cfb0424a52a31bff797e5e92f1180a0b
cs-300 <halfmoonvic@gmail.com>
2017-12-27 10:34:38 +0800

    release(v1): Hello World

--- src/index.js
+++ src/index.js
@@ -1,30 +1,11 @@
-/**** React应用依赖组件 ****/
-// core
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// 样式文件引入
+import './style.scss'
+
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-// Redux 相关
-import { createStore, applyMiddleware, compose } from 'redux'
-import thunk from 'redux-thunk'
-import { Provider } from 'react-redux'
-import reducers from 'store'
-/******* 第三方 组件库 *****/
-/**** 本地公用变量 公用函数 **/
-/******* 本地 公用组件 *****/
-import 'common/css/index.scss'
-/**** 当前组件的 子组件等 ***/
-import App from './App'
 
-const store = createStore(reducers, compose(
-  applyMiddleware(thunk),
-  window.devToolsExtension ? window.devToolsExtension() : () => {}
-))
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
